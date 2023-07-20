702f7820d01b62133597b65a81a163dac451250e
austinaeden <austin.mbogo@student.moringaschool.com>
2023-07-20 18:15:00 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,11 @@
 import React from "react";
+import ArticleList from "./ArticleList";
 import blogData from "../data/blog";
-
-console.log(blogData);
 
 function App() {
   return (
     <div className="App">
+      <ArticleList posts={blogData.posts} />
       You're on your own from here! Follow the deliverables; test things out in
       the browser as you write your code; and good luck!
     </div>
